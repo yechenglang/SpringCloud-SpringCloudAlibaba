4f806224769255277e99399d579b67a62904eff4
yechenglang <yechenglang521@163.com>
2020-10-23 16:19:27 +0800

    + cloud-start cloud-eureka

---
+++ cloud-2020-10-23/cloud-eureka/src/main/java/cn/cloud/ycl/CloudEurekaApplication.java
@@ -0,0 +1,45 @@
+package cn.cloud.ycl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+/**
+ * @author 叶成浪
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class CloudEurekaApplication {
+
+    public static void main(String[] args) {
+        disableAccessWarnings();
+        SpringApplication.run(CloudEurekaApplication.class, args);
+    }
+
+    /**
+     * 忽略非法反射警告  适用于jdk11
+     */
+    @SuppressWarnings("unchecked")
+    public static void disableAccessWarnings() {
+
+        try {
+            Class unsafeClass = Class.forName("sun.misc.Unsafe");
+            Field field = unsafeClass.getDeclaredField("theUnsafe");
+            field.setAccessible(true);
+            Object unsafe = field.get(null);
+
+            Method putObjectVolatile = unsafeClass.getDeclaredMethod("putObjectVolatile", Object.class, long.class, Object.class);
+            Method staticFieldOffset = unsafeClass.getDeclaredMethod("staticFieldOffset", Field.class);
+
+            Class loggerClass = Class.forName("jdk.internal.module.IllegalAccessLogger");
+            Field loggerField = loggerClass.getDeclaredField("logger");
+            Long offset = (Long) staticFieldOffset.invoke(unsafe, loggerField);
+            putObjectVolatile.invoke(unsafe, loggerClass, offset, null);
+        } catch (Exception ignored) {
+        }
+    }
+
+}
---
+++ cloud-2020-10-23/cloud-start/src/main/java/cn/cloud/ycl/Cloud20201023Application.java
@@ -0,0 +1,17 @@
+package cn.cloud.ycl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 叶成浪
+ * @date 2020.10.23
+ */
+@SpringBootApplication
+public class Cloud20201023Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cloud20201023Application.class,args);
+    }
+
+}
