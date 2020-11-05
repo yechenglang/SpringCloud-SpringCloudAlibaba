6e28d2453895e443d3f60d6f63c9cdbc90cca403
yechenglang <yechenglang521@163.com>
2020-11-05 17:43:53 +0800

    all

--- cloud-2020-10-23/cloud-consul-client/cloud-consul-consumer-7010/src/main/java/cn/cloud/ycl/controller/TestController.java
+++ cloud-2020-10-23/cloud-consul-client/cloud-consul-consumer-7010/src/main/java/cn/cloud/ycl/controller/TestController.java
@@ -1,6 +1,7 @@
 package cn.cloud.ycl.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
