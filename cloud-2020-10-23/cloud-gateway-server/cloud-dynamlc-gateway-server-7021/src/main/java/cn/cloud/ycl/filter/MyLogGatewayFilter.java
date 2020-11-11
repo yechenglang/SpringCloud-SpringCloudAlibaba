6a220dcb96466c5b84404a77af862dd40bbe634d
yechenglang <yechenglang521@163.com>
2020-11-11 15:13:39 +0800

    Gateway Global Filter

---
+++ cloud-2020-10-23/cloud-gateway-server/cloud-dynamlc-gateway-server-7021/src/main/java/cn/cloud/ycl/filter/MyLogGatewayFilter.java
@@ -0,0 +1,35 @@
+package cn.cloud.ycl.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.Calendar;
+
+@Component
+@Slf4j
+public class MyLogGatewayFilter implements GlobalFilter, Ordered {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        log.info("***************come in MyLogGatewayFilter:" + Calendar.getInstance().getTime());
+        String username = exchange.getRequest().getQueryParams().getFirst("username");
+        if (StringUtils.isBlank(username)) {
+            log.info("*******非法用户*******");
+            exchange.getResponse().setStatusCode(HttpStatus.NOT_ACCEPTABLE);
+            return exchange.getResponse().setComplete();
+        }
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
